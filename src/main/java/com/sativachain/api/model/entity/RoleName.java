26c66f3b0b2c7ac00276be3ff5d68dd867dfa5d6
NamroN <normanargiolas@gmail.com>
2019-06-07 19:02:58 +0200

    first

---
+++ src/main/java/com/sativachain/api/model/entity/RoleName.java
@@ -0,0 +1,7 @@
+package com.sativachain.api.model.entity;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_PM,
+    ROLE_ADMIN
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo(id, username, description, target_date, is_done)
+values (10001, 'norman','Learn JPA', now(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (10002, 'norman','Learn Data JPA', now(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (10003, 'norman','Learn Microservices', now(), false);
+
+insert into todo(id, username, description, target_date, is_done)
+values (10004, 'norman','Learn Spring Security', now(), false);
